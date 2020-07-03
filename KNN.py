1d014e3d113465e90bc3401bf0c4f64e4a5411bf
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 09:03:46 +0100

    Data Visualization and analysis: 1. Calculating the outcomes of the classes available 2. Displaying a histogram of the income column

--- KNN.py
+++ KNN.py
@@ -8,13 +8,18 @@ import matplotlib.ticker as ticker
 from sklearn import preprocessing
 %matplotlib inline
 
-df = pd.read_csv(teleCust1000f.csv)
-df.head()
-
 # The target field, called custcat, has four possible values that correspond to the four customer groups, as follows:
 #  1- Basic Service 
 #  2- E-Service 
 #  3- Plus Service 
 #  4- Total Service
 
+df = pd.read_csv(teleCust1000f.csv)
+df.head()
 
+#Data visualization and analysis 
+
+#we count how many different outcome classes are available in the custcat field
+df['custcat'].value_counts()
+#we also plot an histogram of the data at the income column at a 50 interval
+df.hist(column='income', bins=50)
