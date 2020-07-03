169f29ad3915aebc767dafff0fab0454ab458609
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 08:57:20 +0100

    Explaining the target field values to keep us focused in the project

--- KNN.py
+++ KNN.py
@@ -11,6 +11,10 @@ from sklearn import preprocessing
 df = pd.read_csv(teleCust1000f.csv)
 df.head()
 
-
+# The target field, called custcat, has four possible values that correspond to the four customer groups, as follows:
+#  1- Basic Service 
+#  2- E-Service 
+#  3- Plus Service 
+#  4- Total Service
 
 
