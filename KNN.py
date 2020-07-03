e771fe90a3de2a696f37bdb1b65031d29efe0bcf
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-03 09:19:15 +0100

    Comments well added, %matplotlib removed and to be used in only jupyter notebooks, filepath bug removed. Code Tested and is running.

--- KNN.py
+++ KNN.py
@@ -1,3 +1,4 @@
+#==========================Importing needed packages and libraries======================
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.ticker as ticker
 from sklearn import preprocessing
-%matplotlib inline
+#%matplotlib inline  #this is necessary in jupyter notebooks not Visual Studio Code, PC terminal or IDE's.
 
 # The target field, called custcat, has four possible values that correspond to the four customer groups, as follows:
 #  1- Basic Service 
@@ -14,12 +15,16 @@ from sklearn import preprocessing
 #  3- Plus Service 
 #  4- Total Service
 
-df = pd.read_csv(teleCust1000f.csv)
-df.head()
-
-#Data visualization and analysis 
-
+#============================Reading the data into the project================================p
+df = pd.read_csv("./teleCust1000t.csv")
+datashow = df.head(10)
+print(datashow)
+#=============================Data visualization and analysis====================================
 #we count how many different outcome classes are available in the custcat field
 df['custcat'].value_counts()
 #we also plot an histogram of the data at the income column at a 50 interval
 df.hist(column='income', bins=50)
+
+
+
+
